70dd59efcbc2334671312163dcf7f65b9b9f1698
Joff Thyer <jsthyer@thyer.org>
2015-10-30 13:01:32 -0400

    added extra filtering params

--- Get-Amcache.ps1
+++ Get-Amcache.ps1
@@ -15,6 +15,13 @@ The Amcache registry hive file to load.  Defaults to \Windows\AppCompat\Programs
 .PARAMETER DestRegKey
 The destination registry key to load the registry hive to.  Defaults to HKLM:\amcache
 
+.PARAMETER Since
+The historic cutoff date used to select the registry hive objects.   All objects are
+selected from that date up through today.
+
+.PARAMETER Ext
+Specify what file extension you want to match.
+
 #>
 
     [CmdletBinding()]
@@ -25,15 +32,27 @@ The destination registry key to load the registry hive to.  Defaults to HKLM:\am
             [Parameter(HelpMessage="Destination registry key to load amcache hive to")]
             [String]$DestRegKey = "HKLM\amcache",
 
-            [Parameter(HelpMessage="Last N number of objects")]
-            [Int]$LastN = 10
+            [Parameter(HelpMessage="Amount of amcache history cutoff date.  Defaults to 90 days back.")]
+            [ValidatePattern("^\d{1,2}/\d{1,2}/\d{4}$")]
+            [String]$Since = (Get-Date).AddDays(-90).ToString('MM/dd/yyyy'),
+
+            [Parameter(HelpMessage="Specify the file extension to match.  Matches all by default.")]
+            [ValidatePattern("^exe|dll|sys$")]
+            [String]$Ext = "*",
+
+            [Parameter(HelpMessage="Specify a filename to match.")]
+            [String]$Filename="*",
+
+            [Switch]$Descending = $false
         )
 
     try {
         reg.exe load $DestRegKey $RegHive | Out-Null
         $rootfile = $DestRegKey.replace("\", ":") + "\Root\File"
-        $amcache = Get-ChildItem -Recurse -Path $rootfile
-        $amcache | Select-Object -Last $LastN | Get-ItemProperty | `
+        if (-not [IO.Path]::GetExtension($Filename)) { $Filename = $Filename + "*"}
+        $sortparams = @{ Property = "TimestampLastModified" }
+        if ($Descending) { $sortparams.Descending = $true }
+        Get-ChildItem -Recurse -Path $rootfile | Get-ItemProperty | `
             foreach {
                 $ts_created = ""
                 $ts_lastmodified = ""
@@ -45,26 +64,30 @@ The destination registry key to load the registry hive to.  Defaults to HKLM:\am
                 if ($_.12) { $ts_created = [DateTime]::FromFileTime($_.12) }
                 if ($_.17) { $ts_lastmodified = [DateTime]::FromFileTime($_.17)}
 
-                New-Object psobject -Property @{
-                    ProductName = $_.0
-                    CompanyName = $_.1
-                    FileVersionNo = $_.2
-                    LangCode = $_.3
-                    SwitchBackContext = $_.4
-                    FileVersion = $_.5
-                    FileSize = $_.6
-                    PEHeaderImageSize = $_.7
-                    PEHeaderHash = $_.8
-                    PEHeaderChecksum = $_.9
-                    FileDescription = $_.c
-                    TimestampCompile = $ts_compile
-                    TimestampCreated = $ts_created
-                    FilePath = $_.15
-                    TimestampLastModified = $ts_lastmodified
-                    ProgramID = $_.100
-                    SHA1Hash = $_.101
+                if ([IO.Path]::GetExtension($_.15) -like "." + $Ext `
+                    -and [IO.Path]::GetFilename($_.15) -like $Filename) {
+                    New-Object psobject -Property @{
+                        ProductName = $_.0
+                        CompanyName = $_.1
+                        FileVersionNo = $_.2
+                        LangCode = $_.3
+                        SwitchBackContext = $_.4
+                        FileVersion = $_.5
+                        FileSize = $_.6
+                        PEHeaderImageSize = $_.7
+                        PEHeaderHash = $_.8
+                        PEHeaderChecksum = $_.9
+                        FileDescription = $_.c
+                        TimestampCompile = $ts_compile
+                        TimestampCreated = $ts_created
+                        FilePath = $_.15
+                        TimestampLastModified = $ts_lastmodified
+                        ProgramID = $_.100
+                        SHA1Hash = $_.101
+                    }
                 }
-            } | Sort-Object $_.17
+            } | Where { $_.TimestampLastModified -gt $Since } `
+                    | Sort-Object @sortparams
     }
     catch {
         $ErrorMessage = $_.Exception.Message
@@ -72,7 +95,6 @@ The destination registry key to load the registry hive to.  Defaults to HKLM:\am
 	    break
     }
     finally {
-        $amcache.Handle.close()
         [gc]::collect()
         [gc]::WaitForPendingFinalizers()
         reg.exe unload $DestRegKey | Out-Null
